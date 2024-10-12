e2bc2fa56cd352652ff88ff0f83100ed9cfb0076
Eri Kurti <82323663+ErjonKurti@users.noreply.github.com>
2024-10-12 18:49:36 +0200

    Update DataTime.js

--- src/components/DataTime/DataTime.js
+++ src/components/DataTime/DataTime.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './DataTime.css'
+import './DataTime.css';
 
 const DataTime = () => {
   const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
@@ -8,11 +8,13 @@ const DataTime = () => {
 
   const day = daysOfWeek[currentDate.getDay()];
   const date = currentDate.toLocaleDateString('en-GB', { day: 'numeric', month: 'long', year: 'numeric' });
+  const time = currentDate.toLocaleTimeString('en-GB', { hour: '2-digit', minute: '2-digit', hour12: false });
 
   return (
     <div>
       <h1 className='day'>{day.toUpperCase()}</h1>
       <p className='data'>{date.toUpperCase()}</p>
+      <p className='time'>{time}</p>
     </div>
   );
 };
